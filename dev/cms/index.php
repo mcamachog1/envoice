f6d7d73e45bae9d11cd4af4aae51e7ed59aaf1cf
Maryvi <mcamachog@gmail.com>
2022-05-16 14:35:33 -0400

    update index

--- dev/cms/index.php
+++ dev/cms/index.php
@@ -1,4 +1,11 @@
-
+<?php
+    $id = "login";
+    if (isset($_REQUEST["id"]))
+        $id = $_REQUEST["id"];
+    $sid = "main";
+    if (isset($_REQUEST["sid"]))
+        $sid = $_REQUEST["sid"];
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -8,13 +15,26 @@
     <link href="./fonts/fontawesome.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/main.css">
     <link href="./img/favicon.png" rel="icon" type="image/png" rel="shortcut icon">
-
+    <?php
+        if (file_exists("./css/".$id.".css"))
+            echo ('<link rel="stylesheet" href="./css/'.$id.'.css">');
+        if (file_exists("./css/".$id."/".$sid.".css"))
+            echo ('<link rel="stylesheet" href="./css/'.$id."/".$sid.'.css">');
+    ?>
     <script type="text/javascript" src="./js/main.js"></script>
-
+    <?php
+        if (file_exists("./js/".$id.".js"))
+            echo ('<script type="text/javascript" src="./js/'.$id.'.js"></script>');
+        if (file_exists("./js/".$id."/".$sid.".js"))
+            echo ('<script type="text/javascript" src="./js/'.$id.'/'.$sid.'.js"></script>');
+    ?>
     <title>DaycoPrint - CMS</title>
 </head>
 <body>
-
+<?php
+    if (file_exists("./content/".$id.".php"))
+        include_once("./content/".$id.".php");
+?>
 <script type="text/javascript" src="./js/all.js"></script>
 </body>
 </html>
