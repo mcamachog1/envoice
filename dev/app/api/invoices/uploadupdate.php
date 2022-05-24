c5a5089b47c37b9d7b406edec71be8478ad21900
Maryvi <mcamachog@gmail.com>
2022-05-23 23:10:40 -0400

    app/api/invoices/uploadupdate.php

--- dev/app/api/invoices/uploadupdate.php
+++ dev/app/api/invoices/uploadupdate.php
@@ -17,7 +17,7 @@ if (!$rs = $db->query($sql))
   badEnd("500", array("sql"=>$sql,"msg"=>$db->error));     
 if ($row = $rs->fetch_assoc())
   $serie = $row['serie'];
-// Asignar ctrnumber inicial según la serie
+// Asignar ctrnumber inicial de la corrida según la serie
 $ctrnumber = getNextControl($serie,$customerid,$db)-1;
 
 // Copiar registros a la tabla definitiva
@@ -44,19 +44,46 @@ $sql = "INSERT INTO invoiceheader ( " .
  
 if (!$db->query($sql))
   badEnd("500", array("sql"=>$sql,"msg"=>$db->error)); 
-  //print_r ($db->affected_rows);
-if ($db->affected_rows == 0)  
-  badEnd("400", array("msg"=>"0 facturas insertadas")); 
- // Copiar registros de detalle
-$sql = "INSERT INTO invoicedetails (invoiceid, itemref, itemdsc, qty, unitprice, itemtax, itemdiscount) " .
-" SELECT         FH.id, D.itemref, D.itemdsc, D.qty, D.unitprice, D.itemtax, D.itemdiscount ".
-" FROM         loadinvoicedetail D ".
-" INNER JOIN    loadinvoiceheader H ".
-" ON            D.loadinvoiceheaderid = H.id ".
-" INNER JOIN     invoiceheader FH ".
-" ON            H.type = FH.type ".
-" AND            H.refnumber = FH.refnumber ".
-" WHERE        H.customerid=$customerid ".
-" AND            FH.customerid=$customerid ";
 
-badEnd("400", array("msg"=>$sql));
+if ($db->affected_rows == 0)  
+  badEnd("400", array("msg"=>"0 facturas insertadas"));
+
+$qtyinvoices =  $db->affected_rows;
+
+// Copiar registros de detalle
+$sql = "INSERT INTO invoicedetails (invoiceid, itemref, itemdsc, qty, unitprice, itemtax, itemdiscount) " .
+  " SELECT         FH.id, D.itemref, D.itemdsc, D.qty, D.unitprice, D.itemtax, D.itemdiscount ".
+  " FROM         loadinvoicedetail D ".
+  " INNER JOIN    loadinvoiceheader H ".
+  " ON            D.loadinvoiceheaderid = H.id ".
+  " INNER JOIN     invoiceheader FH ".
+  " ON        H.refnumber = FH.refnumber    ".
+  " WHERE        H.customerid=$customerid ".
+  " AND            FH.customerid=$customerid ".
+  " AND  POSITION(H.type IN FH.type) > 0 "; 
+
+if (!$db->query($sql))
+  badEnd("500", array("sql"=>$sql,"msg"=>$db->error)); 
+
+// Actualizar next control
+$series = getSeries($customerid,$db);
+$index=array_search($serie, $series);
+if (is_null($index))
+    badEnd("400", array("msg"=>"Serie no existe para este cliente"));
+// Ubicar el next control
+$nexts=getNextControls($customerid,$db);
+$next = $nexts[$index];
+// Construir nextcontrol en string
+$nexts[$index] = $next + $qtyinvoices;
+$str_nextcontrol = implode("-",$nexts);
+// Se actualiza el nextcontrol del cliente
+$update = "UPDATE customers SET nextcontrol = '$str_nextcontrol' WHERE id=$customerid ";
+if (!$db->query($update)) 
+  badEnd("500", array("sql"=>$sql,"msg"=>$db->error)); 
+
+// Salida
+$out = new stdClass(); 
+$out->recordsupdates = $qtyinvoices;
+header("HTTP/1.1 200");
+echo (json_encode($out));
+die();  
