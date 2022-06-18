f155feac1dd3b7bee06c6e29dc6aab11e37b869c
Maryvi <mcamachog@gmail.com>
2022-06-18 11:31:32 -0400

    pull setseniatusers

--- dev/cms/api/preferences/setseniatusers.php
+++ dev/cms/api/preferences/setseniatusers.php
@@ -5,60 +5,91 @@ header("Content-Type:application/json");
 include("../../../settings/dbconn.php");
 include("../../../settings/utils.php");
 
-function insertUsers($users,$passwords,$db){
+function getUsers($db){
+  $sql = "SELECT name, value FROM preferences";
+  if (!$rs=$db->query($sql))
+    badEnd("500", array("sql"=>$sql,"msg"=>$db->error));
+  $records=array();
+// Serialize
+  while ($row = $rs->fetch_assoc()){
+    if(filter_var($row['value'], FILTER_VALIDATE_EMAIL))       {
+        $record = new stdClass();
+        $record->id = $row['name'];
+        $record->value = $row['value'];
+        $records[] = $record;
+    }
+  }
+  return $records;
+}
+
+function insertUsers($users,$emails,$db){
     $counter=0;
     $a_users = explode("-",$users);
-    $a_passwords = explode("-",$passwords);
+    $a_emails = explode("-",$emails);
     $max_users=count($a_users);
+    // Seleccionar usuarios que ya existen
+    $usersinserted=getUsers($db);   
+    // Si el usuario a insertar no existe se inserta
     for ($i=0; $i<$max_users;$i++) {
-        $sql = "INSERT INTO preferences (name, value) VALUES ('".$a_users[$i]."','".$a_passwords[$i]."')";
-        if (!$db->query($sql))
-            badEnd("500", array("sql"=>$sql,"msg"=>$db->error));    
-        $counter++;
+        $insert=true;
+        foreach ($usersinserted as $inserted){
+            if ($a_emails[$i]==$inserted->value){
+                $insert=false;
+                break;
+            }
+        }
+        if ($insert){    
+            $sql = "INSERT INTO preferences (name, value) VALUES ('".$a_users[$i]."','".$a_emails[$i]."')";
+            if (!$db->query($sql))
+                badEnd("500", array("sql"=>$sql,"msg"=>$db->error));    
+            $counter++;
+        }
     }
     return $counter;
 }
-function checkInput($users,$passwords){
+function checkInput($users,$emails){
     $a_users = explode("-",$users);
-    $a_passwords = explode("-",$passwords);
+    $a_emails = explode("-",$emails);
     $max_users = count($a_users);
-    $max_pwd = count($a_passwords);
-    if ($max_pwd!=$max_users)
-        badEnd("400", array("msg"=>"Cantidad de usuarios debe ser igual a cantidad de passwords"));
+    $max_emails = count($a_emails);
+    if ($max_emails!=$max_users)
+        badEnd("400", array("msg"=>"La cantidad de usuarios debe ser igual a la cantidad de emails"));
     foreach ($a_users as $user)
         if (strlen($user)==0)
-            badEnd("400", array("msg"=>"Nombre de usuario no puede ser vacio"));
-    foreach ($a_passwords as $password)
-        if (strlen($password)==0)
-            badEnd("400", array("msg"=>"Password no puede ser vacio"));
+            badEnd("400", array("msg"=>"El nombre del usuario no puede estar vacío"));
+    foreach ($a_emails as $email){
+        avoidinjection($email,'email');
+        if (strlen($email)==0)
+            badEnd("400", array("msg"=>"El email no puede estar vacío"));
+        
+    }
     return true;
 }
-function deleteUsers($users,$db){
-    $a_users = explode("-",$users);
-    $list="";
-    $max = count($a_users);
-    for ($i=0;$i<$max-1;$i++)
-        $list .= "'".$a_users[$i]."',";
-    $list .= "'".$a_users[$max-1]."'";
-    $sql = "DELETE FROM preferences WHERE name IN ($list)";
+function deleteUsers($emails,$db){
+    $condition="0";
+    $a_emails= explode("-",$emails);
+    
+    foreach ($a_emails as $email)
+        $condition .= " OR name LIKE '$email%' OR value = '$email'";
+    $sql = "DELETE FROM preferences WHERE NAME NOT IN (SELECT NAME FROM (SELECT NAME FROM preferences WHERE $condition) AS t )";
     if (!$db->query($sql))
-        badEnd("500", array("sql"=>$sql,"msg"=>$db->error));    
+        badEnd("500", array("sql"=>$sql,"msg"=>$db->error));        
     return $db->affected_rows;
 }
 // Parametros obligatorios
-$parmsob = array("users","passwords","sessionid");
+$parmsob = array("usernames","emails","sessionid");
 if (!parametrosValidos($_GET, $parmsob))
-    badEnd("400", array("msg"=>"Parametros obligatorios " . implode(", ", $parmsob)));
+    badEnd("400", array("msg"=>"Parámetros obligatorios " . implode(", ", $parmsob)));
 // Validar sesion
 $userid = isSessionValidCMS($db, $_REQUEST["sessionid"]);
 // Revisar parametros
-$users = avoidInjection($_GET["users"],'list');
-$passwords = avoidInjection($_GET["passwords"],'list');
-checkInput($users,$passwords);
-deleteUsers($users,$db);
+$users = avoidInjection($_GET["usernames"],'list');
+$emails = avoidInjection($_GET["emails"],'list');
+checkInput($users,$emails);
+deleteUsers($emails,$db);
 // Salida
 $out = new stdClass;    
-$out->count =insertUsers($users,$passwords,$db);
+$out->count =insertUsers($users,$emails,$db);
 header("HTTP/1.1 200");
 echo (json_encode($out));
 die();      
