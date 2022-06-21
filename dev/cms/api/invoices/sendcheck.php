2797ff314c6aacfb745d1c5d29e4acc3d782847c
Maryvi <mcamachog@gmail.com>
2022-06-21 14:50:44 -0400

    move app/api/send and app/api/sendcheck

--- dev/cms/api/invoices/sendcheck.php
+++
@@ -1,9 +0,0 @@
-<?php
-// seniat/api/invoices/sendcheck.php
-
-    header("Content-Type:application/json");
-    include("../../../settings/dbconn.php");
-    include("../../../settings/utils.php");
-    require '../../../hooks/PHPMailer5/PHPMailerAutoload.php';
-
-?>
